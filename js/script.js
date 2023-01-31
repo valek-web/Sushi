c6ffe38f97d297536b6b56747cc6419b70df43ef
valek-web <ziryanovvalentin@mail.ru>
2023-02-01 02:06:03 +0300

    headerA

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-const dropdown = document.querySelector(".dropdown");
-const dropdown_cl = document.querySelector(".dropdown_cl");
+/* toggleMenu */
 
-const dropdown__menu = document.querySelector(".dropdown__menu");
-const dropdownMenuC = document.querySelector(".dropdownMenuC");
+const close_menu = document.querySelector(".close-menu");
+const open_menu = document.querySelector(".open-menu");
+const menu__wrapper = document.querySelector(".menu__wrapper");
+
+open_menu.addEventListener("click", () => {
+  menu__wrapper.classList.add("menu__wrapper-active");
+});
+
+close_menu.addEventListener("click", () => {
+  menu__wrapper.classList.remove("menu__wrapper-active");
+});
+
+/* varText */
 
 const var_text = document.querySelector(".promo__var");
 const mapText = ["Whoever.", "Whenever.", "Wherever."];
-let value_html = 1
+let value_html = 1;
 
 setInterval(() => {
-  var_text.innerHTML = mapText[value_html]
+  var_text.innerHTML = mapText[value_html];
   if (value_html == 2) {
-    value_html = 0
+    value_html = 0;
   } else {
-    value_html++
+    value_html++;
   }
-}, 1500)
+}, 1500);
+
+/* dropdownLogo */
+
+const dropdown = document.querySelector(".dropdown");
+const dropdown_cl = document.querySelector(".dropdown_cl");
 
 dropdown.addEventListener("click", () => {
   dropdown_cl.classList.add("dropdown_cl__active");
@@ -33,6 +48,10 @@ window.addEventListener("click", (event) => {
     }
   }
 });
+/* dropdownMenu */
+
+const dropdown__menu = document.querySelector(".dropdown__menu");
+const dropdownMenuC = document.querySelector(".dropdownMenuC");
 
 dropdown__menu.addEventListener("click", () => {
   dropdownMenuC.classList.add("dropdownMenuC__active");
@@ -48,5 +67,25 @@ window.addEventListener("click", (event) => {
         openDropdown.classList.remove("dropdownMenuC__active");
       }
     }
+  }
+});
+
+/* addBorderHeader */
+
+let scrollpos = window.scrollY;
+
+const header = document.querySelector(".header");
+const scrollChange = 54;
+
+const add_class_on_scroll = () => header.classList.add("header-border");
+const remove_class_on_scroll = () => header.classList.remove("header-border");
+
+window.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= scrollChange) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
   }
 });
